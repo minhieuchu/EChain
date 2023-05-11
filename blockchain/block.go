01b3fc0762fa1f9dae3139ba57e81a03b53fb88a
Hieu Chu <minhieuchu@gmail.com>
2023-05-11 21:10:35 +0700

    Add Mine method to Block & function to create Genesis block

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,51 @@
 package blockchain
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"math/big"
+	"time"
+)
+
 type Block struct {
 	Transactions []*Transaction
 	Timestamp    string
 	PrevHash     []byte
 	Hash         []byte
 	Nonce        int
+}
+
+func (block *Block) Mine() {
+	nonce := 1
+	for {
+		encodedBlock := bytes.Join(
+			[][]byte{
+				Encode(block.Transactions),
+				[]byte(block.Timestamp),
+				block.PrevHash,
+				Encode(nonce),
+			},
+			[]byte{},
+		)
+		blockHash := sha256.Sum256(encodedBlock)
+		hashValue := new(big.Int).SetBytes(blockHash[:])
+
+		if hashValue.Cmp(TARGET_HASH) == -1 {
+			block.Nonce = nonce
+			block.Hash = blockHash[:]
+			break
+		}
+		nonce++
+	}
+}
+
+func Genesis(toAddress string) *Block {
+	coinbaseTransaction := CoinBaseTransaction(toAddress)
+	block := Block{
+		Transactions: []*Transaction{coinbaseTransaction},
+		Timestamp:    time.Now().String(),
+		PrevHash:     []byte{},
+	}
+	block.Mine()
+	return &block
 }
--- blockchain/utils.go
+++ blockchain/utils.go
@@ -3,11 +3,16 @@ package blockchain
 import (
 	"bytes"
 	"encoding/gob"
+	"math/big"
 )
 
-const COINBASE_REWARD = 1000 // satoshi
+const hashValueLength = 256 // bits
+const difficultyLevel = 12  // bits
 
-func Encode (value interface{}) []byte {
+const COINBASE_REWARD = 1000 // satoshi
+var TARGET_HASH = new(big.Int).Lsh(big.NewInt(1), hashValueLength-difficultyLevel)
+
+func Encode(value interface{}) []byte {
 	var byteBuffer bytes.Buffer
 	encoder := gob.NewEncoder(&byteBuffer)
 	encoder.Encode(value)
