de0890c5702a9568eab0c302f1de467ff88302bf
Hieu Chu <minhieuchu@gmail.com>
2023-05-13 12:00:30 +0700

    Add InitBlockchain function

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,15 +1,33 @@
 package blockchain
 
 import (
-	badger "github.com/dgraph-io/badger/v3"
+	"log"
+
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
+const myAddress = "My Address"
+
 type BlockChain struct {
-	DataBase badger.DB
+	DataBase *leveldb.DB
 	LastHash []byte
 }
 
 type BlockChainIterator struct {
-	DataBase    badger.DB
+	DataBase    *leveldb.DB
 	CurrentHash []byte
+}
+
+func InitBlockChain() *BlockChain {
+	db, err := leveldb.OpenFile("storage", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	genesisBlock := Genesis(myAddress)
+	db.Put(genesisBlock.Hash, Encode(genesisBlock), nil)
+	db.Put([]byte(LAST_HASH_STOGAGE_KEY), genesisBlock.Hash, nil)
+
+	return &BlockChain{db, genesisBlock.Hash}
 }
--- blockchain/utils.go
+++ blockchain/utils.go
@@ -3,6 +3,7 @@ package blockchain
 import (
 	"bytes"
 	"encoding/gob"
+	"log"
 	"math/big"
 )
 
@@ -10,6 +11,7 @@ const hashValueLength = 256 // bits
 const difficultyLevel = 12  // bits
 
 const COINBASE_REWARD = 1000 // satoshi
+const LAST_HASH_STOGAGE_KEY = "LAST_HASH"
 var TARGET_HASH = new(big.Int).Lsh(big.NewInt(1), hashValueLength-difficultyLevel)
 
 func Encode(value interface{}) []byte {
@@ -17,4 +19,10 @@ func Encode(value interface{}) []byte {
 	encoder := gob.NewEncoder(&byteBuffer)
 	encoder.Encode(value)
 	return byteBuffer.Bytes()
+}
+
+func HandleErr(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
